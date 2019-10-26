1d470dee3578fdd6e2fbb61126fb597ba5555b92
MichielJ1998 <michiel.jonckheere@live.be>
2019-10-26 13:52:43 +0200

    foutje in constructor, copy paste fout wrs

--- src/rental/ReservationSessionBean.java
+++ src/rental/ReservationSessionBean.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 public class ReservationSessionBean extends Session implements ReservationSession {
 
-    public ManagerSessionBean(CarRentalAgency cra) {
+    public ReservationSessionBean(CarRentalAgency cra) {
         super(cra);
         this.quotes = new ArrayList<Quote>();
     }
